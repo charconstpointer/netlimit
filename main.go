9cc8a8b4575ad49c33d5192e85eea0ffd48bf17b
charconstpointer <attwistingnether@gmail.com>
2022-06-10 09:06:50 +0200

    chore: vars

--- main.go
+++ main.go
@@ -1,46 +1,34 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
 	"io"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
 
 	"github.com/charconstpointer/slowerdaddy/slowerdaddy"
 )
 
+var (
+	limitConn  = 100
+	limitTotal = 9999
+	addr       = ":8080"
+	proto      = "tcp"
+	fileName   = "lorem"
+)
+
 func main() {
-	limitConn := 100
-	limitTotal := 9999
-	ln, err := slowerdaddy.Listen("tcp", ":8080", limitTotal, limitConn)
+	ln, err := slowerdaddy.Listen(proto, addr, limitTotal, limitConn)
 	if err != nil {
 		log.Fatal(err)
 	}
 	handler := http.HandlerFunc(func(w http.ResponseWriter, _ *http.Request) {
-		f, err := os.Open("lorem")
+		f, err := os.Open(fileName)
 		if err != nil {
 			log.Fatal(err)
 		}
 		_, _ = io.Copy(w, f)
 	})
 
-	go http.Serve(ln, handler)
-	sc := bufio.NewReader(os.Stdin)
-	for {
-		fmt.Println("Enter a number:")
-		line, err := sc.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		newLimit, err := strconv.Atoi(line[:len(line)-1])
-		if err != nil {
-			log.Println(err)
-			continue
-		}
-		ln.SetConnLimit(newLimit)
-	}
+	http.Serve(ln, handler)
 }
